ab33d0036184debf4aecdda15234ba3f632564c0
Tharanishwaran <133676934+Tharanishwaran@users.noreply.github.com>
2023-12-09 22:38:09 +0530

    Update forloop.cpp

--- forloop.cpp
+++ forloop.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
 
+int main() {
+  // Initialize loop counter
+  int i = 1;
+
+  // Loop 10 times
+  for (i; i <= 10; ++i) {
+    // Print the current number
+    std::cout << i << std::endl;
+  }
+
+  return 0;
+}
