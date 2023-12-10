e0b9d694f831b4cfaa7cf4a35a0ded697d153a7a
Tharanishwaran <133676934+Tharanishwaran@users.noreply.github.com>
2023-12-10 21:43:33 +0530

    Update dowhileloop.cpp

--- dowhileloop.cpp
+++ dowhileloop.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+
+int main() { // main function
+
+  int i = 1; // Initialize the counter variable
+
+  do { // Loop statement
+  
+    std::cout << i << " "; // Print the current number
+    
+    i++; // Increment the counter variable
+  } 
+    while (i <= 10); // Loop condition: check after the loop execution
+
+  std::cout << std::endl; // Print a newline at the end
+
+  return 0;
+}
 
