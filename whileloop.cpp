aaf3ae2883c2add98113a56062082c17e6a21372
Tharanishwaran <133676934+Tharanishwaran@users.noreply.github.com>
2023-12-10 21:39:44 +0530

    Update whileloop.cpp

--- whileloop.cpp
+++ whileloop.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+
+int main() { // main function
+
+  int i = 1; // Initialize the counter variable
+
+
+  while (i <= 10) { // Loop condition: while i is less than or equal to 10
+  
+    std::cout << i << " "; // Print the current number
+    
+    i++; // Increment the counter variable
+  }
+
+  std::cout << std::endl; // Print a newline at the end
+
+  return 0;
+}
 
